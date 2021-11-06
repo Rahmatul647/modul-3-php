bc3e6b6dda7208eb5dfe1de7f466637edce4c3fc
Rahmatul Azizah <79456933+Rahmatul647@users.noreply.github.com>
2021-11-06 11:31:14 +0700

    Add files via upload

---
+++ modul3/latihan2.php
@@ -0,0 +1,7 @@
+<?php
+$nilai = 50;
+if($nilai >= 75){
+    echo "Nilai anda $nilai, Anda LULUS";
+}else{
+    echo "bilai anda $nilai, anda GAGAL";
+}
---
+++ modul3/latihan3.php
@@ -0,0 +1,9 @@
+<?php
+$user ="Rahma";
+$pass ="123";
+if($user == "Rahma" && $pass == "123"){
+    echo "Login Berhasil";
+}else{
+    echo "Login gagal";
+}
+?>
